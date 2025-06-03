04848a2e62b8fd9c25fe1d1717bbf42f2dd913e3
Gerald Joaquin <116679273+Gerald-Joaquin@users.noreply.github.com>
2025-06-04 00:39:31 +0800

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,13 @@
+package Java;
+
+//- Task: Set up your Java development environment (JDK, IDE).
+//- Challenge: Write a "Hello, World!" program.
+
+public class day1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("I am learning Java.");
+		System.out.println(18);
+	}
+}
---
+++ day2.java
@@ -0,0 +1,34 @@
+package Java;
+
+
+//- Task: Learn about primitive data types (int, char, boolean, etc.).
+//- Challenge: Create variables and print them.
+
+public class day2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Gerald";
+		System.out.println(name);
+		
+		int age = 19;
+		System.out.println(age);
+		
+		int num;
+		num = 20;
+		System.out.println(num);
+		
+		int myNum = 15;
+		myNum = 25;
+		System.out.println(num);
+		
+		char letter = 'D'; // must single quote would be use only
+		System.out.println(letter);
+		
+		boolean bool = true;
+		System.out.println(bool);
+		
+		float gpa = 1.25f; //must always use 'f' at the end
+		System.out.println(gpa);
+	}
+}
