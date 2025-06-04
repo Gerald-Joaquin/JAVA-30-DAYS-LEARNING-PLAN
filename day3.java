07daf1caaeb366b87bebe2642f512cf9aa33bb0e
Gerald Joaquin <116679273+Gerald-Joaquin@users.noreply.github.com>
2025-06-04 22:30:21 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,27 @@
+package Java;
+
+//- Task: Learn about arithmetic, relational, and logical operators.
+//- Challenge: Write a program using operators to perform arithmetic calculations.
+
+public class day3 {
+	public static void main(String[] args) {
+		int x = 20;
+		int y = 20;
+		int z = 10;
+		
+		int sum = x + y;
+		int difference = y - z;
+		int product = x * y;
+		int quotient = y/z;
+		int increment = ++x;
+		int decrement = --y;
+		
+		
+		System.out.println("Sum: " + sum);
+		System.out.println("Difference: " + difference);
+		System.out.println("Product: " + product);
+		System.out.println("Quotient: " + quotient);
+		System.out.println("Increment: " + increment);
+		System.out.println("Decrement: " + decrement);
+	}
+}
